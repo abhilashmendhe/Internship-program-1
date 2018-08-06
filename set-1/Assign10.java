83c79d4fa533d6ec976be3606da660618ed99892
abhilashmendhe <31188186+abhilashmendhe@users.noreply.github.com>
2018-08-06 12:38:13 +0530

    changeee

--- set-1/Assign10.java
+++ set-1/Assign10.java
@@ -10,8 +10,8 @@ package set1;
 public class Assign10 {
 
     public static void main(String args[]){
-    		int millimeter = 1000;
-    		double meter = (double) millimeter / 1000;
+    		int millimeter = 20;
+    		/*double meter = (double) millimeter / 1000;
     		double feets = (double) millimeter / 300;
     		double inches = (double) millimeter / 25;
     		double centimeter = (double) millimeter / 100;
@@ -21,8 +21,20 @@ public class Assign10 {
     		System.out.println("millimeter to inches is "+ inches);
     		System.out.println("millimeter to centimeter is "+ centimeter);
 
-    	//	System.out.println("Meter --- Feets --- Inches --- Centimeter");
-    	//	System.out.println((int)meter+" --- "+(int)(millimeter % 1000)/300+" --- "+(int)((millimeter % 1000)%300)/25+" --- "+centimeter);
+    		System.out.println("Meter --- Feets --- Inches --- Centimeter");
+    		System.out.println((int)meter+" --- "+(int)(millimeter % 1000)/300+" --- "+(int)((millimeter % 1000)%300)/25+" --- "+(int)((((millimeter % 1000)%300)%25)/10));
+    	*/
+    int	meter = millimeter / 1000;
+    int	r1 = millimeter % 1000;
+     int 	feets =  r1 / 300;
+      int	r2 = r1 % 300;
+  		int inches = r2 / 25;
+  		int r3 = r2 %  25;
+    	int centimeter = r3 / 10;
+    	  		System.out.println("Meter is "+ meter);
+    		System.out.println("Feets is "+ feets);
+    		System.out.println("Inches is "+ inches);
+    		System.out.println("Centimeter is "+ centimeter);
     	}
 
 }
