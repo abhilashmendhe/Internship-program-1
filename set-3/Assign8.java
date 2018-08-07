ccbaeb1c779a536ef86653f0bf7c58b607ca3bc6
abhilashmendhe <31188186+abhilashmendhe@users.noreply.github.com>
2018-08-07 12:18:50 +0530

    assign8 completed

---
+++ set-3/Assign8.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)Assign8.java
+ *
+ *
+ * @author
+ * @version 1.00 2018/8/7
+ */
+
+package set3;
+public class Assign8 {
+
+   public static void main (String args[]){
+
+   		int b = 4, a = 7, c = 11;
+
+  		double compute = (double)Math.pow(b,2) - ( 4 * a * c );
+  		String ans = ( compute < 0 ) ? "Roots are Imaginary" :
+  			         ( compute > 0 ) ? "Roots are real" : "Roots are equal";
+
+  			         System.out.println(ans);
+   	}
+
+}
