c52d39bb2ac21fc83fc278676f808d6b8b6ce3d6
abhilashmendhe <31188186+abhilashmendhe@users.noreply.github.com>
2018-08-07 08:18:50 +0530

    added assign13

---
+++ set-2/Assign13.java
@@ -0,0 +1,24 @@
+/**
+ * @(#)Assign13.java
+ *
+ *
+ * @author
+ * @version 1.00 2018/8/7
+ */
+
+
+public class Assign13 {
+
+    public static void main(String args[]){
+
+    	double a=5,b=6,c=1;
+    	double sqr = Math.pow(b,2);
+    	double compute1 =  Math.sqrt(sqr - (4 * a * c));
+    	double compute2 =  (-b) + compute1;
+    	double ans =  compute2 / (2 * a);
+
+    	System.out.println("Anwer is "+ans);
+     }
+
+
+}
