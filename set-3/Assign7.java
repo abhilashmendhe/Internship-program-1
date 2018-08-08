076217f378738d91901a4a32719f7cf2c1ce0595
Abhilash Mendhe <ahbilashmendhe@gmail.com>
2018-08-08 22:05:42 +0530

    Slight changes is assign7

--- set-3/Assign7.java
+++ set-3/Assign7.java
@@ -15,7 +15,7 @@ public class Assign7 {
 
 		double p = (double)(a+b+c)/2;
 		double compute = (double)Math.sqrt( p * (p-a) * (p-b) * (p-c));
-    	String ans = ( (a == 0 || b == 0 || c == 0) || (compute == 0) ) ? "Does not form a triangle!!!" : ( a == b && a == c && b == c ) ? "It's an equilateral triangle" :
+    	String ans = ( (a <= 0 || b <= 0 || c <= 0) || (compute == 0) ) ? "Does not form a triangle!!!" : ( a == b && a == c && b == c ) ? "It's an equilateral triangle" :
     		         ( (a == b) || (b == c) || (c == a) ) ? "It's an isosceles triangle" : "It's an scales triangle";
 
         System.out.println(ans);
