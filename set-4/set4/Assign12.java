ae2df8755dde8b5f39d00ab019e5a503cc750d39
abhilashmendhe <31188186+abhilashmendhe@users.noreply.github.com>
2018-08-14 13:32:29 +0530

    slight changes

--- set-4/set4/Assign12.java
+++ set-4/set4/Assign12.java
@@ -11,7 +11,7 @@ import java.util.*;
 public class Assign12 {
 
     public static void main(String args[]){
- 		System.out.println("Enter character ");
+ 		System.out.println("Enter number");
     	Scanner sc = new Scanner(System.in);
     	int num = sc.nextInt();
 		char ch = (char) num;
