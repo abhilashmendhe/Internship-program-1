497208df9f074ab96527f037b38dd0c28e099b9e
abhilashmendhe <31188186+abhilashmendhe@users.noreply.github.com>
2018-08-20 08:25:26 +0530

    set-9 done full

--- set-9/Assign11.java
+++ set-9/Assign11.java
@@ -2,15 +2,25 @@
  * @(#)Assign11.java
  *
  *
- * @author 
+ * @author
  * @version 1.00 2018/8/20
  */
 
 
 public class Assign11 {
 
-    public Assign11() {
+    public static void main(String args[])
+    {
+    	for (int i=5; i>=1; i--)
+    	{
+    		for(int j=1; j<=(5-i); j++)
+    		{
+    			System.out.print(" ");
+    		}
+    		for(int k=i; k>=1;k--){
+				System.out.print(k);
+    		}
+			System.out.println();
+    	}
     }
-    
-    
 }
