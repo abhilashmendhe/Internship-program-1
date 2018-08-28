470c0710aed6b42ca9bdd2dc9b21d7738592d106
abhilashmendhe <31188186+abhilashmendhe@users.noreply.github.com>
2018-08-28 14:37:50 +0530

    little changes

--- set-10/Assign6.java
+++ set-10/Assign6.java
@@ -11,24 +11,27 @@ public class Assign6 {
 
     public static void main(String args[])
     {
-    	int t=0;
-		int add_s=0;
-    	for(int i=1; i<=5; i++)
-    	{
+    	int b=-1;
+		for(int i=1; i<=5; i++)
+		{	int a=i;
+			int x=i+b;
+			for(int j=1; j<=5-i; j++)
+			{
+				System.out.print(" ");
+			}
 
-    		for(int a=5; a>=i; a--)
-    		{
-    			System.out.print(" ");
-    		}
-    		for(int j=1; j<=i; j++)
-    		{
-    			System.out.print(t+j);
-    		}
-    		System.out.println();
-    		t=t+i;
-     		add_s += 1;
-    	}
+			for(int k=1; k<=i; k++)
+			{
+				System.out.print(a++);
+			}
 
+			for(int l=2; l<=i; l++)
+			{
+				System.out.print(x--);
+			}
+			b=b+1;
+			System.out.println();
+		}
     }
 
 
