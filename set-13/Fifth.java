d23b8d24b3dc700b01ddffc1ae1c04b712792c5b
abhilashmendhe <31188186+abhilashmendhe@users.noreply.github.com>
2018-08-29 07:28:42 +0530

    saved

--- set-13/Fifth.java
+++ set-13/Fifth.java
@@ -13,14 +13,14 @@ public class Fifth {
     {
     	int i=1;
     	int n1=24,n2=18;
-    	int n=n1+n2;
 		int j=1;
 
 	int tmp=0;
     while(j<n1 && j<n2)
     {
-    	if(n1%j==0 && n2%j==0)
+    	if(n1%j==0 && n2%j==0){
 			tmp=j;
+    	}
 		j++;
     }
 	System.out.println("GCD is "+tmp);
