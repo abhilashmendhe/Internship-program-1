04139fa6c70dbc9dc4fbb8ee59fe6e2fcc551f72
abhilashmendhe <31188186+abhilashmendhe@users.noreply.github.com>
2018-08-29 07:31:07 +0530

    three no. gcd

--- set-13/Fifth.java
+++ set-13/Fifth.java
@@ -12,13 +12,14 @@ public class Fifth {
     public static void main(String args[])
     {
     	int i=1;
-    	int n1=24,n2=18;
+    	int n1=10,n2=15;
+    	int n3=20;
 		int j=1;
 
 	int tmp=0;
-    while(j<n1 && j<n2)
+    while(j<n1 && j<n2 && j<n3)
     {
-    	if(n1%j==0 && n2%j==0){
+    	if(n1%j==0 && n2%j==0 && n3%j==0){
 			tmp=j;
     	}
 		j++;
