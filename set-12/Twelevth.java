f083fff8d25e77c7b10462e06a79b39daa592f87
abhilashmendhe <31188186+abhilashmendhe@users.noreply.github.com>
2018-09-02 22:45:53 +0530

    set-12 completed

---
+++ set-12/Twelevth.java
@@ -0,0 +1,37 @@
+/**
+ * @(#)Twelevth.java
+ *
+ *
+ * @author
+ * @version 1.00 2018/9/2
+ */
+
+package set12;
+import java.util.*;
+public class Twelevth {
+
+    public static void main(String args[])
+    {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.print("Enter n: ");int n = sc.nextInt();
+    	System.out.print("Enter the value of x: "); double x = sc.nextDouble();
+
+    	int i = 2;
+    	double compute = 1/(x+1);
+    	while(i<=n)
+    	{
+    		if(i%2==0)
+    		{
+    			compute += i/(x+i);
+    		}
+    		else
+    		{
+    			compute -= i/(x+i);
+    		}
+    		i++;
+    	}
+    	System.out.println("Sum of the series is "+compute);
+    }
+
+
+}
