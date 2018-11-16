0614162285ea20ab0d562fa84331fc659167740c
Abhilash Mendhe <abhilashmendhe@gmail.com>
2018-11-16 09:32:37 +0530

    3rd completed of set-33

--- set-33/set33_3/src/set33_3/Set33_3.java
+++ set-33/set33_3/src/set33_3/Set33_3.java
@@ -16,6 +16,9 @@ public class Set33_3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Frm f = new Frm();
+        f.setVisible(true);
+        f.setLocation(700, 300);
     }
     
 }
